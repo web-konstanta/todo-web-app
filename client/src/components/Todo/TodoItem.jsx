dd071b142551176bafec277e2e0a87d3fc259d44
Makarenko Kostya <kochanmujik@gmail.com>
2024-11-24 23:21:38 +0200

    change view for todo statuses

--- client/src/components/Todo/TodoItem.jsx
+++ client/src/components/Todo/TodoItem.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 import Button from '../UI/Button/Button';
 
 const TodoItem = ({ todo }) => {
+	const statusClasses = {
+		complete: 'taskComplete',
+		'in progress': 'taskInProgress',
+		todo: 'taskTodo'
+	}
+
 	return (
 		<tr>
 			<th scope="row">{todo.id}</th>
 			<td>{todo.title}</td>
-			<td>{todo.status}</td>
+			<td>
+				<Button btnClassName={statusClasses[todo.status.toLowerCase()]}>{todo.status}</Button>
+			</td>
 			<td>{todo.createdAt}</td>
 			<td>
 				<Button btnClassName="editTask">Edit</Button>
