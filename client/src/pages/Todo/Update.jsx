fd18f872b5f85c420738cbdde1d8a8be7f658e09
Makarenko Kostya <kochanmujik@gmail.com>
2024-12-02 21:56:55 +0200

    create update form for todo

---
+++ client/src/pages/Todo/Update.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/UI/Navbar/Navbar';
+import TodoUpdateForm from '../../components/Todo/TodoUpdateForm';
+
+const Update = () => {
+	return (
+		<div>
+			<Navbar />
+			<TodoUpdateForm />
+		</div>
+	);
+};
+
+export default Update;
