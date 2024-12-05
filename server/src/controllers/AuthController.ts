07f1b074dcec46140fbd3c20bcea3e0783622023
Makarenko Kostya <kochanmujik@gmail.com>
2024-12-05 14:08:37 +0200

    create auth controller and dev commands

--- server/src/controllers/AuthController.ts
+++ server/src/controllers/AuthController.ts
@@ -1,14 +1,18 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { validationResult } from 'express-validator'
 
 class AuthController {
-	async signUp(req: Request, res: Response): Promise<any> {
-		const errors = validationResult(req)
-		if (!errors.isEmpty()) {
-			return res.status(422).json(errors)
-		}
+	async signUp(req: Request, res: Response, next: NextFunction): Promise<any> {
+		try {
+			const errors = validationResult(req)
+			if (!errors.isEmpty()) {
+				return res.status(422).json(errors.array())
+			}
 
-		return res.json({ status: 'success' })
+			return res.json({ status: 'success' })
+		} catch (e) {
+			console.log(e)
+		}
 	}
 }
 
