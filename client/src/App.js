59e49c69ce1b85f9d798631d8a6f3578b80a7dde
Makarenko Kostya <kochanmujik@gmail.com>
2024-12-10 23:31:11 +0200

    create basic auth context

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import { BrowserRouter } from 'react-router-dom';
+import { AuthContext } from './context/authContext';
 import AppRouter from './AppRouter';
-import './App.css'
+import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isAuth, setIsAuth] = useState(false)
+
+  useEffect(() => {
+    if (localStorage.getItem('accessToken')) {
+      setIsAuth(true)
+    }
+  }, [])
+
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <AuthContext.Provider value={{ isAuth, setIsAuth }}>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
